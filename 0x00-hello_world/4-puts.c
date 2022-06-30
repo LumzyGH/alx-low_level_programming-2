4f5b9995dcf13d61996d54bd16d0a449872eb423
lumzygh <adebanjo753@gmail.com>
2022-06-30 08:15:08 -0700

    go

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+
+/**
+ *main - print the phrase "programming is like building a multinigual puzzle"
+ *return: 0 if excited properly, non zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
